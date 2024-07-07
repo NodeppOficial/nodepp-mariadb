f21897568fc44d02422c49c5b6c0954fd3b83cbe
EDBC <becerracenmanueld@gmail.com>
2024-07-07 00:05:35 -0400

    update

--- include/mariadb.h
+++ include/mariadb.h
@@ -7,6 +7,48 @@
 #include <nodepp/nodepp.h>
 #include <nodepp/url.h>
 
+/*────────────────────────────────────────────────────────────────────────────*/
+
+#ifndef NODEPP_MARIADB_GENERATOR
+#define NODEPP_MARIADB_GENERATOR
+
+namespace nodepp { namespace _mariadb_ { GENERATOR( cb ){
+protected:
+    
+    map_t<string_t,string_t> arguments;
+    array_t<string_t> col;
+    int num_fields, x;
+    MYSQL_ROW row;
+
+public:
+
+    template< class T, class U, class V > gnEmit( T& fd, U& res, V& cb ){
+    gnStart 
+
+        if( res == NULL ) { process::error( mysql_error(fd) ); }
+
+        num_fields = mysql_num_fields( res ); 
+        row        = mysql_fetch_row( res );
+
+        for( x=0; x<num_fields; x++ )
+           { col.push( row[x] ); }
+
+        while((row=mysql_fetch_row(res))){
+          for( x=0; x<num_fields; x++ ){
+               arguments[ col[x] ] = row[x] ? row[x] : "NULL"; 
+        } cb ( arguments ); coNext; }
+
+        mysql_free_result( res );
+
+    gnStop
+    }
+
+};}}
+
+#endif
+
+/*────────────────────────────────────────────────────────────────────────────*/
+
 namespace nodepp { using sql_item_t = map_t<string_t,string_t>; }
 
 namespace nodepp { class mariadb_t {
@@ -16,29 +58,6 @@ protected:
         MYSQL *fd = nullptr;
         int state = 1;
     };  ptr_t<NODE> obj;
-
-    template< class T > void callback( T& cb ) const { 
-        MYSQL_RES *res = mysql_store_result(obj->fd);
-        sql_item_t arguments; array_t<string_t> col;
-
-        if( res == NULL ) { //mysql_free_result( res );
-            string_t message = mysql_error(obj->fd);
-            process::error( message );
-        }
-
-        int num_fields = mysql_num_fields( res );
-        MYSQL_ROW row  = mysql_fetch_row( res );
-
-        for( int x=0; x<num_fields; x++ )
-           { col.push( row[x] ); }
-
-        while( (row=mysql_fetch_row(res)) ){
-          for( int x=0; x<num_fields; x++ ){
-               arguments[ col[x] ] = row[x] ? row[x] : "NULL"; 
-        } cb ( arguments ); }
-
-        mysql_free_result( res );
-    }
 
 public:
     
@@ -73,7 +92,7 @@ public:
         char* crt = ssl->get_crt_path().get(); mysql_ssl_set( obj->fd, key, crt, NULL , NULL, NULL );
 
         if( mysql_real_connect( obj->fd, host.get(), user.get(), pass.get(), name.get(), port, NULL, 0 ) == NULL ){
-            string_t message = mysql_error( obj->fd ); process::error( message );
+            process::error( mysql_error( obj->fd ) );
         }
 
     }
@@ -92,7 +111,7 @@ public:
         auto port = url::port( uri );
 
         if( mysql_real_connect( obj->fd, host.get(), user.get(), pass.get(), name.get(), port, NULL, 0 ) == NULL ){
-            string_t message = mysql_error( obj->fd ); process::error( message );
+            process::error( mysql_error( obj->fd ) );
         }
 
     }
@@ -101,17 +120,20 @@ public:
 
     void exec( const string_t& cmd, const function_t<void,sql_item_t>& cb ) const {
         if( mysql_real_query( obj->fd, cmd.data(), cmd.size() ) != 0 ){
-            string_t message = mysql_error( obj->fd );
-            process::error( message );
-        }   callback( cb );
+            process::error( mysql_error( obj->fd ) );
+        }   MYSQL_RES *res = mysql_store_result( obj->fd );   
+
+        _mariadb_::cb task; process::add( task, obj->fd, res, cb );
     }
 
-    array_t<sql_item_t> exec( const string_t& cmd ) const { array_t<sql_item_t> res;
-        function_t<void,sql_item_t> cb = [&]( sql_item_t args ){ res.push( args ); };
+    array_t<sql_item_t> exec( const string_t& cmd ) const { array_t<sql_item_t> arr;
+        function_t<void,sql_item_t> cb = [&]( sql_item_t args ){ arr.push( args ); };
+
         if( mysql_real_query( obj->fd, cmd.data(), cmd.size() ) != 0 ){
-            string_t message = mysql_error( obj->fd );
-            process::error( message );
-        }   callback( cb ); return res;
+            process::error( mysql_error( obj->fd ) );
+        }   MYSQL_RES *res = mysql_store_result( obj->fd );   
+
+        _mariadb_::cb task; process::await( task, obj->fd, res, cb ); return arr;
     }
 
 };}
